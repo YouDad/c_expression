fd41e4dee345cd3385f84ecd799a0479f801e254
luwh0708 <luwh0708@thundersoft.com>
2020-11-24 11:54:26 +0800

    test: refactor

--- test/test.c
+++ test/test.c
@@ -1,7 +1,10 @@
 #include "header.h"
 
-#define test(calc_expression, result, retval) \
-	_test(calc_expression, sizeof(calc_expression), result, retval, __LINE__);
+#define test(calc_expression) \
+	_test(#calc_expression, sizeof(#calc_expression), calc_expression, 0, __LINE__)
+
+#define test_fail(calc_expression) \
+	_test(#calc_expression, sizeof(#calc_expression), 0, 1, __LINE__)
 
 bool _test(const char *calc, int calc_len, int calc_result, int retval, int line)
 {
@@ -26,123 +29,124 @@ bool _test(const char *calc, int calc_len, int calc_result, int retval, int line
 int main()
 {
 	bool ret = true;
-	ret = ret && test("0",     0, 0);
-	ret = ret && test("0/0",   0, 1);
-	ret = ret && test("0/1",   0, 0);
-	ret = ret && test("1+8*3", 25, 0);
-	ret = ret && test("1-8*3", -23, 0);
-	ret = ret && test("1+8/3", 3, 0);
-	ret = ret && test("1-8/3", -1, 0);
-	ret = ret && test("1+(2+3)*4", 21, 0);
-	ret = ret && test("a=1", 1, 0);
-	ret = ret && test("a", 1, 0);
-	ret = ret && test("b=(a=a*2)+2", 4, 0);
-	ret = ret && test("a", 2, 0);
-	ret = ret && test("b", 4, 0);
-	ret = ret && test("c=-a", -2, 0);
-	ret = ret && test("c", -2, 0);
-	ret = ret && test("(a+b)/(c)", -3, 0);
+	int a, b, c;
+	ret = ret && test(0);
+	ret = ret && test_fail(0/0);
+	ret = ret && test(0/1);
+	ret = ret && test(1+8*3);
+	ret = ret && test(1-8*3);
+	ret = ret && test(1+8/3);
+	ret = ret && test(1-8/3);
+	ret = ret && test(1+(2+3)*4);
+	ret = ret && test(a=1);
+	ret = ret && test(a);
+	ret = ret && test(b=(a=a*2)+2);
+	ret = ret && test(a);
+	ret = ret && test(b);
+	ret = ret && test(c=-a);
+	ret = ret && test(c);
+	ret = ret && test((a+b)/(c));
 
 	// !, +, -, ~, ++, --
-	ret = ret && test("!2", 0, 0);
-	ret = ret && test("!+2", 0, 0);
-	ret = ret && test("!-2", 0, 0);
-	ret = ret && test("!0", 1, 0);
-	ret = ret && test("~0", -1, 0);
-	ret = ret && test("++c", -1, 0);
-	ret = ret && test("--c", -2, 0);
+	ret = ret && test(!2);
+	ret = ret && test(!+2);
+	ret = ret && test(!-2);
+	ret = ret && test(!0);
+	ret = ret && test(~0);
+	ret = ret && test(++c);
+	ret = ret && test(--c);
 
-	ret = ret && test("!2 * 0", 0, 0);
-	ret = ret && test("!+2 * 0", 0, 0);
-	ret = ret && test("!-2 * 0", 0, 0);
-	ret = ret && test("!0 + 1", 2, 0);
-	ret = ret && test("~1 + 1", -1, 0);
-	ret = ret && test("++c + 1", 0, 0);
-	ret = ret && test("--c + 1", -1, 0);
+	ret = ret && test(!2 * 0);
+	ret = ret && test(!+2 * 0);
+	ret = ret && test(!-2 * 0);
+	ret = ret && test(!0 + 1);
+	ret = ret && test(~1 + 1);
+	ret = ret && test(++c + 1);
+	ret = ret && test(--c + 1);
 
 	// %
-	ret = ret && test("1%2", 1, 0);
-	ret = ret && test("3%2", 1, 0);
-	ret = ret && test("3%0", 0, 1);
-	ret = ret && test("c%3", -2, 0);
-	ret = ret && test("c%3 + 2", 0, 0);
-	ret = ret && test("c%~1 + 2", 2, 0);
+	ret = ret && test(1%2);
+	ret = ret && test(3%2);
+	ret = ret && test_fail(3%0);
+	ret = ret && test(c%3);
+	ret = ret && test(c%3 + 2);
+	ret = ret && test(c%~1 + 2);
 
 	// <<, >>
-	ret = ret && test("1<<0", 1 << 0, 0);
-	ret = ret && test("1<<20", 1 << 20, 0);
-	ret = ret && test("100>>0", 100 >> 0, 0);
-	ret = ret && test("100>>4", 100 >> 4, 0);
+	ret = ret && test(1<<0);
+	ret = ret && test(1<<20);
+	ret = ret && test(100>>0);
+	ret = ret && test(100>>4);
 
-	ret = ret && test("c = 1<<0", 1 << 0, 0);
-	ret = ret && test("c + 1<<20", 2 << 20, 0);
-	ret = ret && test("c + 100>>0", 101 >> 0, 0);
-	ret = ret && test("c = 100>>4", 100 >> 4, 0);
+	ret = ret && test(c = 1<<0);
+	ret = ret && test(c + 1<<20);
+	ret = ret && test(c + 100>>0);
+	ret = ret && test(c = 100>>4);
 
 	// <, <=, >, >=
-	ret = ret && test("1 < 1", 1 < 1, 0);
-	ret = ret && test("1 <= 1", 1 <= 1, 0);
-	ret = ret && test("1 > 1", 1 > 1, 0);
-	ret = ret && test("1 >= 1", 1 >= 1, 0);
+	ret = ret && test(1 < 1);
+	ret = ret && test(1 <= 1);
+	ret = ret && test(1 > 1);
+	ret = ret && test(1 >= 1);
 
-	ret = ret && test("1 << 1 < 1", 1 << 1 < 1, 0);
-	ret = ret && test("1 << 1 <= 1", 1 << 1 <= 1, 0);
-	ret = ret && test("1 << 1 > 1", 1 << 1 > 1, 0);
-	ret = ret && test("1 << 1 >= 1", 1 << 1 >= 1, 0);
+	ret = ret && test(1 << 1 < 1);
+	ret = ret && test(1 << 1 <= 1);
+	ret = ret && test(1 << 1 > 1);
+	ret = ret && test(1 << 1 >= 1);
 
-	ret = ret && test("c = 1 < 1", 1 < 1, 0);
-	ret = ret && test("c = 1 <= 1", 1 <= 1, 0);
-	ret = ret && test("c = 1 > 1", 1 > 1, 0);
-	ret = ret && test("c = 1 >= 1", 1 >= 1, 0);
+	ret = ret && test(c = 1 < 1);
+	ret = ret && test(c = 1 <= 1);
+	ret = ret && test(c = 1 > 1);
+	ret = ret && test(c = 1 >= 1);
 
 	// ==, !=
-	ret = ret && test("1 == 1", 1 == 1, 0);
-	ret = ret && test("1 != 1", 1 != 1, 0);
+	ret = ret && test(1 == 1);
+	ret = ret && test(1 != 1);
 
-	ret = ret && test("1 << 1 == 1", 1 << 1 == 1, 0);
-	ret = ret && test("1 << 1 != 1", 1 << 1 != 1, 0);
-	ret = ret && test("c = 1 == 1", 1 == 1, 0);
-	ret = ret && test("c = 1 != 1", 1 != 1, 0);
+	ret = ret && test(1 << 1 == 1);
+	ret = ret && test(1 << 1 != 1);
+	ret = ret && test(c = 1 == 1);
+	ret = ret && test(c = 1 != 1);
 
 	// &, ^, |
-	ret = ret && test("5 & 3", 5 & 3, 0);
-	ret = ret && test("1 + 4 & 3", 5 & 3, 0);
-	ret = ret && test("c = 5 & 3", 5 & 3, 0);
+	ret = ret && test(5 & 3);
+	ret = ret && test(1 + 4 & 3);
+	ret = ret && test(c = 5 & 3);
 
-	ret = ret && test("5 ^ 3", 5 ^ 3, 0);
-	ret = ret && test("1 + 4 ^ 3", 5 ^ 3, 0);
-	ret = ret && test("c = 5 ^ 3", 5 ^ 3, 0);
+	ret = ret && test(5 ^ 3);
+	ret = ret && test(1 + 4 ^ 3);
+	ret = ret && test(c = 5 ^ 3);
 
-	ret = ret && test("1 + 4 | 3", 5 | 3, 0);
-	ret = ret && test("5 | 3", 5 | 3, 0);
-	ret = ret && test("c = 5 | 3", 5 | 3, 0);
+	ret = ret && test(1 + 4 | 3);
+	ret = ret && test(5 | 3);
+	ret = ret && test(c = 5 | 3);
 
-	ret = ret && test("1 ^ 3 & 2", 1 ^ 3 & 2, 0);
-	ret = ret && test("4 | 3 & 2", 4 | 3 & 2, 0);
-	ret = ret && test("1 | 1 ^ 1", 1 | 1 ^ 1, 0);
+	ret = ret && test(1 ^ 3 & 2);
+	ret = ret && test(4 | 3 & 2);
+	ret = ret && test(1 | 1 ^ 1);
 
 	// &&, ||
-	ret = ret && test("1 && 1", 1 && 1, 0);
-	ret = ret && test("0 && 1", 0 && 1, 0);
-	ret = ret && test("0 && 0", 0 && 0, 0);
-	ret = ret && test("1 || 1", 1 || 1, 0);
-	ret = ret && test("0 || 1", 0 || 1, 0);
-	ret = ret && test("0 || 0", 0 || 0, 0);
+	ret = ret && test(1 && 1);
+	ret = ret && test(0 && 1);
+	ret = ret && test(0 && 0);
+	ret = ret && test(1 || 1);
+	ret = ret && test(0 || 1);
+	ret = ret && test(0 || 0);
 
-	ret = ret && test("1 ^ 1 && 1", 1 ^ 1 && 1, 0);
-	ret = ret && test("1 ^ 0 && 1", 1 ^ 0 && 1, 0);
-	ret = ret && test("1 ^ 0 && 0", 1 ^ 0 && 0, 0);
-	ret = ret && test("1 ^ 1 || 1", 1 ^ 1 || 1, 0);
-	ret = ret && test("1 ^ 0 || 1", 1 ^ 0 || 1, 0);
-	ret = ret && test("1 ^ 0 || 0", 1 ^ 0 || 0, 0);
+	ret = ret && test(1 ^ 1 && 1);
+	ret = ret && test(1 ^ 0 && 1);
+	ret = ret && test(1 ^ 0 && 0);
+	ret = ret && test(1 ^ 1 || 1);
+	ret = ret && test(1 ^ 0 || 1);
+	ret = ret && test(1 ^ 0 || 0);
 
-	ret = ret && test("c = 1 && 1", 1 && 1, 0);
-	ret = ret && test("c = 0 && 1", 0 && 1, 0);
-	ret = ret && test("c = 0 && 0", 0 && 0, 0);
-	ret = ret && test("c = 1 || 1", 1 || 1, 0);
-	ret = ret && test("c = 0 || 1", 0 || 1, 0);
-	ret = ret && test("c = 0 || 0", 0 || 0, 0);
+	ret = ret && test(c = 1 && 1);
+	ret = ret && test(c = 0 && 1);
+	ret = ret && test(c = 0 && 0);
+	ret = ret && test(c = 1 || 1);
+	ret = ret && test(c = 0 || 1);
+	ret = ret && test(c = 0 || 0);
 
-	ret = ret && test("1 || 1 && 0", 1 || 1 && 0, 0);
+	ret = ret && test(1 || 1 && 0);
 	return ret ? 0 : 1;
 }
